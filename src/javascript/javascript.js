67760c355571a3d637061d72914f6add83c01b2e
omid.esmaeili <omidesmaeeli4@gmail.com>
2024-02-12 15:44:39 +0330

    Add speed

--- src/javascript/javascript.js
+++ src/javascript/javascript.js
@@ -1,7 +1,9 @@
 const player = new Plyr('#player', {
     seekTime: 2,
+    speed: {
+        selected: 1,
+        options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4]
+    }
 });
 
 player.play(); // Start playback
-
-// player.fullscreen.enter(); // Enter fullscreen
